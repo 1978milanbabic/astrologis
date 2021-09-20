ec1fb422c2c8aeff8fe0847d8a7e4ab9d812189a
Your NameMilan <1978milan.babic@gmail.com>
2021-09-21 00:28:03 +0200

    [init] additional setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,30 +1,13 @@
 import React, { useState } from 'react'
-
-// semantic ui
-import {
-  Container,
-  Segment,
-  Header
-} from 'semantic-ui-react'
-
-
 // styles
-import 'semantic-ui-css/semantic.min.css'
-import './assets/App.css'
+import './assets/App.scss'
 
 const App = () => {
 
   return (
-    <Container>
-      <Segment attached='top'>
-        <Header>
-          Edit index.html (public folder) to change title, logo, fonts...
-        </Header>
-      </Segment>
-      <Segment attached='bottom'>
-        Dont forget to edit all details in package.json
-      </Segment>
-    </Container>
+    <>
+
+    </>
   )
 }
 
