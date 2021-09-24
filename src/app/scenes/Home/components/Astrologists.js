99c0c247e04214cbb228c5ee35d7bc3ae29d1ac3
1978milanbabic <1978milan.babic@gmail.com>
2021-09-24 04:15:10 +0200

    [section astrologists] almost done

--- src/app/scenes/Home/components/Astrologists.js
+++ src/app/scenes/Home/components/Astrologists.js
@@ -1,13 +1,82 @@
 import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel'
 
 // import images
-// import textPic from '../../../../../public/assets/tekst.png'
-// import amuPic from '../../../../../public/assets/token.png'
+import callOk from '../../../../../public/assets/call-ok.png'
+import callNok from '../../../../../public/assets/call-nok.png'
+
+// import astros
+import astrologs from '../utils/astologysts'
 
 const Astrologists = props => {
   return (
     <section className='astrologists'>
+      <h2>IZABERI SVOG ASTROLOGA</h2>
+      <p></p>
 
+      <div style={{
+        width: '100%',
+        maxWidth: '1500px',
+        overflowX: 'hidden',
+        margin: '0 auto',
+        overflowY: 'auto',
+        paddingBottom: '50px'
+      }}>
+        <div className='astro-slide-cont'>
+          <Carousel
+            infiniteLoop={true}
+            centerMode
+            centerSlidePercentage={50}
+            showArrows={false}
+            showStatus={false}
+            showIndicators={true}
+            showThumbs={false}
+            autoPlay={true}
+            stopOnHover={false}
+            interval={4000}
+          >
+            {astrologs && astrologs.length > 0 && astrologs.map(astro => (
+              <div key={astro.code} className='slide-member-cont'>
+                <div className='img-cont'>
+                  <div className='img-wrapper'>
+                    <img alt='' src={astro.imgSrc} />
+                  </div>
+                </div>
+                <div className='name-table'>
+                  <div className='left'>
+                    <p><span>{astro.name}</span> - Kod - {astro.code}</p>
+                  </div>
+                  <div className='right'>
+                    <p>
+                      <img alt='' src={astro.available ? callOk : callNok} />
+                      Tarot savetnik je {astro.available ? ('slobodan') : ('zauzet')}
+                      </p>
+
+                  </div>
+                </div>
+                <div className='osavet'>
+                  <p><span>O SAVETNIKU</span>: {astro.about}</p>
+                </div>
+                <div className='tehn'>
+                  <p><span>TEHNIKE</span>: {astro.techniques}</p>
+                </div>
+                <a
+                  href={`tel: ${astro.phone}`}
+                  className={astro.available ? 'av' : 'non-av'}
+                >
+                  <span className='left'>
+                    Broj telefona: {astro.phone}
+                  </span>
+                  <span className='right'>
+                    {astro.price} RSD/min
+                  </span>
+                </a>
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </div>
     </section>
   )
 }
---
+++ src/app/scenes/Home/utils/astologysts.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+// import images
+import astro1 from '../../../../../public/assets/astrolog1-100.jpg'
+import astro2 from '../../../../../public/assets/astrolog2-100.jpg'
+import astro3 from '../../../../../public/assets/astrolog3-100.jpg'
+import astro4 from '../../../../../public/assets/astrolog4.jpg'
+
+const astrologs = [
+  {
+    imgSrc: astro1,
+    name: 'Eleonora',
+    code: '84',
+    available: true,
+    about: 'Moje godine rada sa tarot kartama pokazale su mi da se uz pomoć alternativnih tehnika svaka životna situacija može promeniti, biranjem boljeg i srećnijeg puta',
+    techniques: 'tarot, numerologija, duhovna rešavanja, ljubavna predviđanja, bioenergija, psihološki razgovori.',
+    phone: '0900-000-000',
+    price: 150
+  },
+  {
+    imgSrc: astro3,
+    name: 'Konstantin',
+    code: '34',
+    available: true,
+    about: 'Pomažem Vam u pogledu na Vašu sadašnjost, a uz pomoć tarota videćemo gde i kako krenuti u budućnost.',
+    techniques: 'tarot, sudbinske karte, numerologija, sudbinske karte, psihološki razgovori, bioenergija.',
+    phone: '0900-000-000',
+    price: 150
+  },
+  {
+    imgSrc: astro4,
+    name: 'Bogdana',
+    code: '59',
+    available: true,
+    about: 'Za sve Vaše probleme obratite mi se sa poverenjem jer moje dugogodišnje iskustvo u pomaganju kroz ezoteriju može Vam samo pomoći.',
+    techniques: 'tumačenje snova, sudbinske karte, tarot, vidovidost, skidanje uroka, energija na daljinu.',
+    phone: '0900-000-000',
+    price: 150
+  },
+  {
+    imgSrc: astro2,
+    name: 'Sofija',
+    code: '73',
+    available: false,
+    about: 'Stručnjak za tarot i numerologiju. Bavim se pomaganjem ljudima na polju ljubavi, finansija, porodičnih problema i raznih psihičkih stanja energijom na daljinu.',
+    techniques: 'energija na daljinu, ljubav, finansije, energetsko čišćenje, numerologija, tarot, slanje pozitivne energije.',
+    phone: '0900-000-000',
+    price: 150
+  },
+]
+
+export default astrologs
