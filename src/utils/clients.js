d6f98bf7e801116919bdced5a04730c137e1f99d
milanbabic-munogu <milanbabic@munogu.com>
2021-09-29 22:14:19 +0200

    [comments] edited

--- src/utils/clients.js
+++ src/utils/clients.js
@@ -1,44 +1,39 @@
 import React from 'react'
 
 // import images
-import client1 from '../../public/assets/klijent.jpg'
-
+import client1 from '../../public/assets/comment1_1.jpg'
+import client2 from '../../public/assets/comment1_2.jpg'
+import client3 from '../../public/assets/comment1_3.jpg'
+import client4 from '../../public/assets/comment1_4.jpg'
 
 const clients = [
   {
-    name: 'Selena1',
-    years: 23,
+    name: 'Selena',
+    years: 71,
     location: 'Beograd',
     imageSrc: client1,
-    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, expedita distinctio itaque, perferendis id doloribus sapiente nisi provident inventore asperiores odit accusamus exercitationem in nobis quibusdam est voluptas tempore fuga!'
+    comment: 'Tarot savetnik Konstantin mi je potpuno otvorio oci i sada mi je sve jasno. Prosto ne mogu da verujem koju energiju on ima i koliko snage i pozitivnosti moze da unese u neciji zivot.'
   },
   {
-    name: 'Selena2',
+    name: 'Marija',
     years: 23,
-    location: 'Beograd',
-    imageSrc: client1,
-    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, expedita distinctio itaque, perferendis id doloribus sapiente nisi provident inventore asperiores odit accusamus exercitationem in nobis quibusdam est voluptas tempore fuga!'
+    location: 'Novi Sad',
+    imageSrc: client2,
+    comment: 'Eleonora je prava vila koja kao da je svo vreme bila oko mene i posmatrala moj zivot od pocetka do ovog momenta sada. Neverovatno je koje razumevanje postoji i koja snaga zraci iz nje same. HVALA ELEONORA.'
   },
   {
-    name: 'Selena3',
-    years: 23,
-    location: 'Beograd',
-    imageSrc: client1,
-    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, expedita distinctio itaque, perferendis id doloribus sapiente nisi provident inventore asperiores odit accusamus exercitationem in nobis quibusdam est voluptas tempore fuga!'
+    name: 'Stevan',
+    years: 64,
+    location: 'Kragujevac',
+    imageSrc: client3,
+    comment: 'Sofija je pravi Tarot Mag. Resenje za moje probleme se konacno prikazalo pred mojim ocima. Zahvalila bih se Magdaleni na spoznaji o kojoj nikada nisam ni  mastala.'
   },
   {
-    name: 'Selena4',
-    years: 23,
-    location: 'Beograd',
-    imageSrc: client1,
-    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, expedita distinctio itaque, perferendis id doloribus sapiente nisi provident inventore asperiores odit accusamus exercitationem in nobis quibusdam est voluptas tempore fuga!'
-  },
-  {
-    name: 'Selena5',
-    years: 23,
-    location: 'Beograd',
-    imageSrc: client1,
-    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, expedita distinctio itaque, perferendis id doloribus sapiente nisi provident inventore asperiores odit accusamus exercitationem in nobis quibusdam est voluptas tempore fuga!'
+    name: 'Vesna',
+    years: 34,
+    location: 'Subotica',
+    imageSrc: client4,
+    comment: 'Nikola je moj ANDJEO CUVAR. Zivot mi je promenio u prve tri minute razgovora a sve nakon toga bio je samo slag na torti. Jos uvek ne mogu da verujem kojom brzino i preciznoscu je gledao kroz mene i moje probleme.'
   }
 ]
 
