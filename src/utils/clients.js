6e90557f93f58141694ae0fbb51c0038af537c9f
milanbabic-munogu <milanbabic@munogu.com>
2021-09-29 22:57:38 +0200

    [testimonials] edit

--- src/utils/clients.js
+++ src/utils/clients.js
@@ -12,28 +12,28 @@ const clients = [
     years: 71,
     location: 'Beograd',
     imageSrc: client1,
-    comment: 'Tarot savetnik Konstantin mi je potpuno otvorio oci i sada mi je sve jasno. Prosto ne mogu da verujem koju energiju on ima i koliko snage i pozitivnosti moze da unese u neciji zivot.'
+    comment: 'Tarot savetnik Konstantin mi je potpuno otvorio oči i sada mi je sve jasno. Prosto ne mogu da verujem koju energiju on ima i koliko snage i pozitivnosti može da unese u nečiji život.'
   },
   {
     name: 'Marija',
     years: 23,
     location: 'Novi Sad',
     imageSrc: client2,
-    comment: 'Eleonora je prava vila koja kao da je svo vreme bila oko mene i posmatrala moj zivot od pocetka do ovog momenta sada. Neverovatno je koje razumevanje postoji i koja snaga zraci iz nje same. HVALA ELEONORA.'
+    comment: 'Eleonora je prava vila koja kao da je svo vreme bila oko mene i posmatrala moj život od početka do ovog momenta sada. Neverovatno je koje razumevanje postoji i koja snaga zrači iz nje same. HVALA ELEONORA.'
   },
   {
     name: 'Stevan',
     years: 64,
     location: 'Kragujevac',
     imageSrc: client3,
-    comment: 'Sofija je pravi Tarot Mag. Resenje za moje probleme se konacno prikazalo pred mojim ocima. Zahvalila bih se Magdaleni na spoznaji o kojoj nikada nisam ni  mastala.'
+    comment: 'Sofija je pravi Tarot Mag. Rešenje za moje probleme se konačno prikazalo pred mojim očima. Zahvalio bih se Magdaleni na spoznaji o kojoj nikada nisam ni maštao.'
   },
   {
     name: 'Vesna',
     years: 34,
     location: 'Subotica',
     imageSrc: client4,
-    comment: 'Nikola je moj ANDJEO CUVAR. Zivot mi je promenio u prve tri minute razgovora a sve nakon toga bio je samo slag na torti. Jos uvek ne mogu da verujem kojom brzino i preciznoscu je gledao kroz mene i moje probleme.'
+    comment: 'Bogdana je moj ANDJEO CUVAR. Život mi je promenila u prve tri minute razgovora a sve nakon toga bio je samo šlag na torti. Još uvek ne mogu da verujem kojom brzino i preciznošću je gledao kroz mene i moje probleme.'
   }
 ]
 
