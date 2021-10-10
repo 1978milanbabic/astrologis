13f280e38ecc581d19128ea0497b2a80a160e77f
1978milanbabic <1978milan.babic@gmail.com>
2021-10-10 23:09:22 +0200

    [Header] topo slider removed

--- src/app/scenes/components/Header.js
+++ src/app/scenes/components/Header.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import { useHistory } from 'react-router'
-import { useSpring, animated } from "react-spring"
 
 // import Callinfo
 import CallInfo from "./CallInfo"
@@ -11,61 +10,23 @@ import logo from '../../../../public/assets/logo.png'
 const Header = props => {
   // states
   const [screenSize, setScreenSize] = useState()
-  const [keyTopSlider1, setKeyTopSlider1] = useState(1)
-  const [keyTopSlider2, setKeyTopSlider2] = useState(400)
-
-  // top slider
-  const scrolling1 = useSpring({
-    from: { transform: "translate(80%,0)" },
-    to: { transform: "translate(-80%,0)" },
-    config: { duration: 20000 },
-    reset: true,
-    onRest: () => {
-      setKeyTopSlider1(keyTopSlider1 + 1)
-    }
-  })
-  const scrolling2 = useSpring({
-    from: { transform: "translate(80%,0)", opacity: 1 },
-    to: { transform: "translate(-80%,0)", opacity: 1 },
-    config: { duration: 20000 },
-    reset: true,
-    delay: 10000,
-    onRest: () => {
-      setKeyTopSlider2(keyTopSlider2 + 1)
-    }
-  })
 
   // switch screen sizes
   useEffect(() => {
     const media = csstojsID('css-connector')
 
-    let sliderTimeout
-
     media.size(1, () => {
       setScreenSize('desktop')
-      // show second animated text for desktop
-      sliderTimeout = setTimeout(() => {
-        let secText = document.getElementsByClassName('second-anim')[0]
-        if (secText) secText.classList.remove('second-anim')
-      }, 10020)
     })
 
     media.size(2, () => {
       setScreenSize('mobile')
-      // remove slider
-      let navDet = document.getElementsByClassName('nav-details')[0]
-      let divs = navDet.getElementsByTagName('div')
-      if (divs.length > 1) {
-        let wanted = divs[divs.length - 1]
-        navDet.innerHTML(wanted)
-      }
     })
 
     media.init()
 
     // cleanup on unload
     return () => {
-      clearTimeout(sliderTimeout)
       window.removeEventListener('resize', () => {})
     }
 
@@ -96,21 +57,7 @@ const Header = props => {
         </div>
       </nav>
       <div className='nav-details'>
-        {screenSize === 'desktop' && (
-          <>
-            <div key={keyTopSlider1}>
-              <animated.div style={scrolling1}>
-                <CallInfo device='desktop' />
-              </animated.div>
-            </div>
-            <div key={keyTopSlider2} className='second-anim'>
-              <animated.div style={scrolling2}>
-                <CallInfo device='desktop' />
-              </animated.div>
-            </div>
-          </>
-        )}
-        {screenSize === 'mobile' && (<CallInfo device='mobile' />)}
+        <CallInfo device={screenSize} />
       </div>
     </>
   )
