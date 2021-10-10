0f030d910d6bdd53a609e4034e92022ae60e792d
1978milanbabic <1978milan.babic@gmail.com>
2021-10-10 20:39:43 +0200

    [astrologist] edit

--- src/utils/astologysts.js
+++ src/utils/astologysts.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 // import images
-import astro1 from '../../public/assets/astrolog1-100.jpg'
-import astro2 from '../../public/assets/astrolog2-100.jpg'
+import astro1 from '../../public/assets/marija.png'
+import astro2 from '../../public/assets/anika.png'
 // import astro3 from '../../public/assets/astrolog3-100.jpg'
 import astro4 from '../../public/assets/astrolog4.jpg'
 
@@ -12,28 +12,28 @@ const astrologs = [
     name: 'Marija',
     code: '11',
     available: true,
-    about: '***',
-    techniques: 'tarot i numerologija',
+    about: 'Otvaram tarot. Koristim Rajder Vajtov špil. Numerologija, psihološki razgovori..',
+    techniques: 'Tarot i Numerologija',
     phone: '0901-900-907',
     price: 150
   },
   {
     imgSrc: astro4,
     name: 'Kristina',
-    code: '12',
+    code: '22',
     available: true,
     about: 'Kombinacija astrologije i tarota daje odgovore na sva pitanja. Rešenja svih dilema i problema na ljubavom, poslovnom i zdravstvenom planu.',
-    techniques: 'tarot i numerologija',
+    techniques: 'Tarot i Numerologija',
     phone: '0901-900-907',
     price: 150
   },
   {
     imgSrc: astro2,
-    name: 'Anita',
-    code: '13',
+    name: 'Anika',
+    code: '33',
     available: false,
-    about: '***',
-    techniques: '***',
+    about: 'Neko je rekao da je Astrologija ČITAC BOZIJEG RUKOPISA a ona je način mog života vec 40 godina! Specijalnost mi je Natalna Astrologija i ona je osnov svega! Takodje radim Progresivnu Astrologiju a lično volim Lunarni Horoskop! Astro Tarot je takodje nešto što mi predstavlja izazov ali kao dopunu Astrologiji! Ja Vam probleme neću resiti ,ali ću Vas usmeriti na najbolji mogući put!',
+    techniques: 'Astrologija',
     phone: '0901-900-907',
     price: 150
   },
--- src/utils/clients.js
+++ src/utils/clients.js
@@ -12,28 +12,28 @@ const clients = [
     years: 71,
     location: 'Beograd',
     imageSrc: client1,
-    comment: 'Tarot savetnik Konstantin mi je potpuno otvorio oči i sada mi je sve jasno. Prosto ne mogu da verujem koju energiju on ima i koliko snage i pozitivnosti može da unese u nečiji život.'
+    comment: 'Tarot savetnica Marija mi je potpuno otvorio oči i sada mi je sve jasno. Prosto ne mogu da verujem koju energiju ona ima i koliko snage i pozitivnosti može da unese u nečiji život.'
   },
   {
     name: 'Marija',
     years: 23,
     location: 'Novi Sad',
     imageSrc: client2,
-    comment: 'Eleonora je prava vila koja kao da je svo vreme bila oko mene i posmatrala moj život od početka do ovog momenta sada. Neverovatno je koje razumevanje postoji i koja snaga zrači iz nje same. HVALA ELEONORA.'
+    comment: 'Anika je prava vila koja kao da je svo vreme bila oko mene i posmatrala moj život od početka do ovog momenta sada. Neverovatno je koje razumevanje postoji i koja snaga zrači iz nje same. HVALA ANIKA.'
   },
   {
     name: 'Stevan',
     years: 64,
     location: 'Kragujevac',
     imageSrc: client3,
-    comment: 'Sofija je pravi Tarot Mag. Rešenje za moje probleme se konačno prikazalo pred mojim očima. Zahvalio bih se Magdaleni na spoznaji o kojoj nikada nisam ni maštao.'
+    comment: 'Kristina je pravi Tarot Mag. Rešenje za moje probleme se konačno prikazalo pred mojim očima. Zahvalio bih se Kristini na spoznaji o kojoj nikada nisam ni maštao.'
   },
   {
     name: 'Vesna',
     years: 34,
     location: 'Subotica',
     imageSrc: client4,
-    comment: 'Bogdana je moj ANDJEO CUVAR. Život mi je promenila u prve tri minute razgovora a sve nakon toga bio je samo šlag na torti. Još uvek ne mogu da verujem kojom brzinom i preciznošću je gledao kroz mene i moje probleme.'
+    comment: 'Marija je moj ANDJEO CUVAR. Život mi je promenila u prve tri minute razgovora a sve nakon toga bio je samo šlag na torti. Još uvek ne mogu da verujem kojom brzinom i preciznošću je gledala kroz mene i moje probleme.'
   }
 ]
 
